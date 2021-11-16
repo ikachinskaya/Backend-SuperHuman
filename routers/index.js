176cc30fea230cb413898bfc436d86dc7adcd270
Inna <inna.kachinskaya13@gmail.com>
2021-11-16 15:44:43 +0200

    Fix router

--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 const superHumanRouter = require("./superHumanRouter");
 
-router.use("/superhumans", superHumanRouter);
+router.use("/superhumans",  superHumanRouter);
 
 module.exports = router;
--- routers/superHumanRouter.js
+++ routers/superHumanRouter.js
@@ -2,6 +2,22 @@ const superHumanRouter = require("express").Router();
 
 const SuperHumanController = require("../controllers/superHumanControllers");
 
+const { findSuperHuman } = require("../middlewares/superHumanMW");
+
 superHumanRouter.post("/", SuperHumanController.createSuperHuman);
+
+superHumanRouter.patch(
+  "/:id",
+  findSuperHuman,
+  SuperHumanController.updateSuperHuman
+);
+
+superHumanRouter.delete(
+  "/:id",
+  findSuperHuman,
+  SuperHumanController.deleteSuperHuman
+);
+
+superHumanRouter.get("/", SuperHumanController.getSuperHumans);
 
 module.exports = superHumanRouter;
